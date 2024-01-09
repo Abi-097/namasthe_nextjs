1677dca78fad15d8f7fbdd94a2bfbaeaa44262f7
Abi-097 <120549516+Abi-097@users.noreply.github.com>
2024-01-10 02:23:45 +0530

    inital started

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        white: "#fafafa",
+        green: "#769863",
+        grey: "#F8F8F8",
+        black: "#212121",
+        lightblack: "#343434",
+        DBlack: "#413F3E",
+      },
+      animation: {
+        fade: "fadeIn .5s ease-in-out",
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      keyframes: {
+        fadeIn: {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
